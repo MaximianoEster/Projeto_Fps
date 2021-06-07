9a5f2e3fabffe58d744d142b1863d2fb9ce51534
Ester Maximiano de Andrade Silva <estermaximiano25@gmail.com>
2021-06-07 18:56:40 -0300

    new file:   .idea/.idea.Projeto - Fps/.idea/vcs.xml 	new file:   Assets/Data/VFX/Object Pool.meta 	new file:   Assets/Data/VFX/Object Pool/VfxObjectPoolData.asset 	new file:   Assets/Data/VFX/Object Pool/VfxObjectPoolData.asset.meta 	new file:   Assets/Data/VFX/Simples Prefab.meta 	new file:   Assets/Data/VFX/Simples Prefab/Vfx Cartoon Explosion Data.asset 	new file:   Assets/Data/VFX/Simples Prefab/Vfx Cartoon Explosion Data.asset.meta 	new file:   Assets/Data/VFX/Simples Prefab/Vfx Dark Death Data.asset 	new file:   Assets/Data/VFX/Simples Prefab/Vfx Dark Death Data.asset.meta 	new file:   Assets/Data/VFX/Simples Prefab/Vfx Fire Data.asset 	new file:   Assets/Data/VFX/Simples Prefab/Vfx Fire Data.asset.meta 	new file:   Assets/Data/VFX/Simples Prefab/Vfx Light Death Data.asset 	new file:   Assets/Data/VFX/Simples Prefab/Vfx Light Death Data.asset.meta 	new file:   Assets/Data/VFX/Simples Prefab/Vfx Simple Explosion Data.asset 	new file:   Assets/Data/VFX/Simples Prefab/Vfx Simple Explosion Data.asset.meta 	new file:   Assets/Data/VFX/Simples Prefab/Vfx Spawn Knight Data.asset 	new file:   Assets/Data/VFX/Simples Prefab/Vfx Spawn Knight Data.asset.meta 	new file:   Assets/Data/VFX/Simples Prefab/Vfx Spawn Skeleton Data.asset 	new file:   Assets/Data/VFX/Simples Prefab/Vfx Spawn Skeleton Data.asset.meta 	new file:   Assets/Data/VFX/Simples Prefab/Vfx Spawn Wizard Data.asset 	new file:   Assets/Data/VFX/Simples Prefab/Vfx Spawn Wizard Data.asset.meta 	new file:   Assets/Prefabs/VFX/Vfx_Dark_Death.prefab 	new file:   Assets/Prefabs/VFX/Vfx_Dark_Death.prefab.meta 	new file:   Assets/Prefabs/VFX/Vfx_Explosion_Simple.prefab 	new file:   Assets/Prefabs/VFX/Vfx_Explosion_Simple.prefab.meta 	new file:   Assets/Prefabs/VFX/Vfx_Fire.prefab 	new file:   Assets/Prefabs/VFX/Vfx_Fire.prefab.meta 	new file:   Assets/Prefabs/VFX/Vfx_Light_Death.prefab 	new file:   Assets/Prefabs/VFX/Vfx_Light_Death.prefab.meta 	new file:   Assets/Prefabs/VFX/Vfx_Spawn_Knight.prefab 	new file:   Assets/Prefabs/VFX/Vfx_Spawn_Knight.prefab.meta 	new file:   Assets/Prefabs/VFX/Vfx_Spawn_Skeleton.prefab 	new file:   Assets/Prefabs/VFX/Vfx_Spawn_Skeleton.prefab.meta 	new file:   Assets/Prefabs/VFX/Vfx_Spawn_Wizard.prefab 	new file:   Assets/Prefabs/VFX/Vfx_Spawn_Wizard.prefab.meta 	new file:   Assets/Scripts/Object Pooling/VfxManager.cs 	new file:   Assets/Scripts/Object Pooling/VfxManager.cs.meta

---
+++ Projeto - Fps/Assets/Scripts/Object Pooling/VfxManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VfxManager : MonoBehaviour
+{
+    [SerializeField] private VfxObjectPoolData _vfxObjectPoolData = default;
+    [SerializeField] private Transform _vfxGroup = default;
+    
+    private Dictionary<VfxType, List<Vfx>> 
+        _vfxDictionary = new Dictionary<VfxType, List<Vfx>>();
+    
+    private int _amountVfx = default;
+    
+    private void Start()
+    {
+        InitializeSettings();
+    }
+    
+    public Vfx GetVfxFromPool(VfxType type)
+    {
+        if (_vfxDictionary[type].Count > 0)
+        {
+            Vfx currentVfx = _vfxDictionary[type][_vfxDictionary[type].Count - 1];
+            currentVfx.transform.SetParent(null);
+            _vfxDictionary[type].Remove(currentVfx);
+            return currentVfx;
+        }
+
+        return null;
+    }
+    
+    public void DisableVfx(Vfx currentVfx)
+    {
+        AddVfxToDictionary(currentVfx);
+    }
+    
+    public void InitializeSettings()
+    {
+        _amountVfx = _vfxObjectPoolData.VfxAmount;
+        AddListToDictionary(_vfxObjectPoolData.VfxPrefabsList);
+    }
+    
+    private void AddListToDictionary(List<Vfx> vfxList)
+    {
+        for (int i = 0; i < vfxList.Count; i++)
+        {
+            for (int vfx = 0; vfx < _amountVfx; vfx++)
+            {
+                Vfx vfxInstance = Instantiate(_vfxObjectPoolData.VfxPrefabsList[i]);
+                
+                vfxInstance.DisableVfx += DisableVfx;
+                vfxInstance.InitializeSettings();
+                AddVfxToDictionary(vfxInstance);
+            }
+        } 
+    }
+
+    private void AddVfxToDictionary(Vfx vfxData)
+    {
+        vfxData.gameObject.SetActive(false);
+        vfxData.transform.SetParent(_vfxGroup);
+        
+        if (_vfxDictionary.ContainsKey(vfxData.Type))
+        {
+            _vfxDictionary[vfxData.Type].Add(vfxData);
+        }
+        else
+        {
+            _vfxDictionary.Add(vfxData.Type, new List<Vfx>());
+            _vfxDictionary[vfxData.Type].Add(vfxData);
+        }
+    }
+}
